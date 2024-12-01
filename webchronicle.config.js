e305875d08005fcaaa8334d92f85d2e8132e7f78
Alex Zappa <alex@zappa.dev>
2024-12-01 11:52:03 +0100

    Update URLs and urlFilter in webchronicle.config.js
    Add 'https://example.org' to the urls list and adjust the urlFilter to include it. Also, correct the link for website-scraper options to point to the GitHub readme for more accurate reference.

--- webchronicle.config.js
+++ webchronicle.config.js
@@ -1,13 +1,14 @@
-// More options for website-scraper here https://www.npmjs.com/package/website-scraper
+// More options for website-scraper here https://github.com/website-scraper/node-website-scraper?tab=readme-ov-file#options
 export default {
     urls : [
         'https://example.com',
+        'https://example.org',
     ],
     recursive : true,
     maxRecursiveDepth : 3,
     filenameGenerator : 'bySiteStructure',
     prettifyUrls : true,
     urlFilter : (url) => {
-        return url.startsWith('https://example.com');
+        return url.startsWith('https://example.com') || url.startsWith('https://example.org');
     },
 }
